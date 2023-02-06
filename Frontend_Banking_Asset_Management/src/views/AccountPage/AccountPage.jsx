2c4720d7953f08cc68595d1187919fcc55c89353
unknown <andrey.detochkin@gmail.com>
2023-02-06 15:59:37 +0100

    some changes on about us page

--- Frontend_Banking_Asset_Management/src/views/AccountPage/AccountPage.jsx
+++ Frontend_Banking_Asset_Management/src/views/AccountPage/AccountPage.jsx
@@ -1,13 +1,20 @@
-import React from 'react';
+import React from "react";
+import accountimage from "../../assets/images/db-bank-Open-Account-Image.png";
 
 function AccountPage() {
-  return <div className='account-page'>
-  <div className='account-texts'>
-            <h1 className='acc-header'>Dolphin Bank offers the convenience of mobile banking - without compromising on 
-your values. </h1>
-            <p className='account-page-text'>We use modern technologies that make your everyday life easier and contribute to the 
-future of us all. <button
-            className="acbutton1"
+  return (
+    <div className="account-page">
+    <img className="image-account-page" src={accountimage} />
+      <div className="account-texts">
+        <h1 className="acc-header">
+          Dolphin Bank offers the convenience of mobile banking - without
+          compromising on your values.{" "}
+        </h1>
+        <p className="account-page-text">
+          We use modern technologies that make your everyday life easier and
+          contribute to the future of us all.{" "}
+          <button
+            className="acbutton1 account-button"
             type="button"
             onClick={(e) => {
               e.preventDefault();
@@ -15,10 +22,11 @@ future of us all. <button
             }}
           >
             Sign Up
-          </button></p>
-  
-</div>
-        </div>
+          </button>
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default AccountPage;
