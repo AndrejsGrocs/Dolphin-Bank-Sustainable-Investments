434437a063e249b466725f0a9abeab510c581a6f
unknown <andrey.detochkin@gmail.com>
2023-02-09 11:41:25 +0100

    about us element positioning update

--- Frontend_Banking_Asset_Management/src/components/RobinContactCard/RobinContactCard.jsx
+++ Frontend_Banking_Asset_Management/src/components/RobinContactCard/RobinContactCard.jsx
@@ -1,30 +1,52 @@
-import React from 'react'
-import cardRobin from '../../assets/images/db-bank-AbouUs-Robin.png'
-import aboutuslinkedin from "../../assets/images/db-bank-AbouUs-Icons-LinkedIn.png"
-import aboutusmail from "../../assets/images/db-bank-AbouUs-Icons-Email.png"
-import aboutusgit from "../../assets/images/db-bank-AbouUs-Icons-GitHub.png"
-
-
+import React from "react";
+import cardRobin from "../../assets/images/db-bank-AbouUs-Robin.png";
+import aboutuslinkedin from "../../assets/images/db-bank-AbouUs-Icons-LinkedIn.png";
+import aboutusmail from "../../assets/images/db-bank-AbouUs-Icons-Email.png";
+import aboutusgit from "../../assets/images/db-bank-AbouUs-Icons-GitHub.png";
 
 function RobinContactCard() {
   return (
     <>
-    <div className='cardRobin'>
-                    <img className='image-robin' src={cardRobin} alt='contact person image'/> 
+      <div className="cardRobin">
+        <img
+          className="image-robin"
+          src={cardRobin}
+          alt="contact person image"
+        />
 
-                    <div className='aboutus-card-texts-links'>
-                    <h2 className='about-us-card-header-robin'>Robin Skladny</h2>
-                    <p className='about-us-card-text'>Backend/Frontend</p>
-                    <div className='socialmedia-icons-block'>
-                    
-               <a href="https://www.linkedin.com/in/robin-skladny-647a1b229/" target="_blank"><img className= 'aboutus-socmediaicon'   src={aboutuslinkedin} alt='social media icon'></img></a>
-                <a href="https://github.com/Robynio" target="_blank"><img className= 'aboutus-socmediaicon'   src={aboutusgit} alt='social media icon'></img></a>
-                 <a href="mailto:robin_skladny@gmx.de" ><img className='aboutus-socmediaicon' src={aboutusmail} alt='social media icon'></img></a>
-                 </div>
-                   </div>
-                   </div>
-                   </>
-  )
+        <div className="aboutus-card-texts-links-robin">
+          <h2 className="about-us-card-header-robin">Robin Skladny</h2>
+          <p className="about-us-card-text-robin">Backend/Frontend</p>
+          <div className="socialmedia-icons-block-robin">
+            <a
+              href="https://www.linkedin.com/in/robin-skladny-647a1b229/"
+              target="_blank"
+            >
+              <img
+                className="aboutus-socmediaicon"
+                src={aboutuslinkedin}
+                alt="social media icon"
+              ></img>
+            </a>
+            <a href="https://github.com/Robynio" target="_blank">
+              <img
+                className="aboutus-socmediaicon"
+                src={aboutusgit}
+                alt="social media icon"
+              ></img>
+            </a>
+            <a href="mailto:robin_skladny@gmx.de">
+              <img
+                className="aboutus-socmediaicon"
+                src={aboutusmail}
+                alt="social media icon"
+              ></img>
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default RobinContactCard
+export default RobinContactCard;
